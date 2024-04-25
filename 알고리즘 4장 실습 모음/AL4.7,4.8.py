993810b51dc5ed6430a753f6da978734d5607fb7
kimayeong <cngot0000@naver.com>
2024-04-25 13:11:16 +0900

    a

---
+++ 알고리즘 4장 실습 모음/AL4.7,4.8.py
@@ -0,0 +1,27 @@
+import time             #time 모듈 포함
+
+def slow_power(x,n):    #반복으로 x,n을 구하는 함수
+    result = 1.0
+    for i in range(n):  # 루프:n번 반복
+        result = result * x
+        return result
+    
+def power(x,n):
+    if n==0:        #종료조건
+        return 1        #모든수의 0승은 1
+    elif(n%2)==0:       #n의 짝수
+        return power(x*x,n//2)  #정수의 나눗셈
+    else:                       #n이 홀수
+        return x*power(x*x,(n-1)//2)
+    
+print("     억지기법(2**500)=", power(2.0,500))
+print(" 축소정복기법(2*500)=",slow_power(2.0,500))
+
+t1 = time.time()
+for i in range(100000):power(2.0,500)   #축소 정복 10만회
+t2 = time.time()
+for i in range(100000):slow_power(2.0,500)  #억지 기법 10만회
+t3=time.time()
+
+print("     억지기법 시간...",t3-t2)
+print("축소정복기법 시간...",t2-t1)
